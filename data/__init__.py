ec6281b434fc28bdca0bf26cc6f38ef2d017d340
Mencret <43661631+mbest99@users.noreply.github.com>
2019-06-07 16:08:16 +0700

    Delete __init__.py

--- data/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-from .app import *
-from .facebook import facebook
-
-
-os.system('cls' if os.name == 'nt' else 'clear')
-lol(open(os.path.dirname(os.path.realpath(__file__))+'/__data__/banners.txt', 'r').read())
